924b600368b40ed7b47b300dd7d0580e5c5298a0
SilverNight123 <niteDragon12@outlook.com>
2025-04-04 20:49:56 -0300

    Remove bin

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@
 
 int main(int argc, char* argv[]) {
 
-    Chip8 chip8 = Chip8("chip8-roms/games/Cave.ch8");
+    std::string filepath = "chip8-roms/games/Cave.ch8";
+
+    Chip8 chip8 = Chip8(filepath);
 
     for(int i = 0; i < 356; i++)
     {
